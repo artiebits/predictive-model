c647550966d62dc9cb1f926f318e1ec2e593d905
artiebits <artur.khusaenov@gmail.com>
2021-11-21 21:08:32 +0100

    new script to fetch data

---
+++ R/footballdata.R
@@ -0,0 +1,24 @@
+library(worldfootballR)
+library(lubridate)
+library(dplyr)
+
+countries <- c("ENG", "ESP", "GER", "ITA", "FRA", "RUS")
+
+for (country in countries) {
+  print(paste("Getting data for", country))
+
+  data <- worldfootballR::get_match_results(country = country, gender = "M", season_end_year = 2010:2022)
+
+  fixture <- data %>%
+    filter(Date >= lubridate::today()) %>%
+    select(Date, Time, Home, Away)
+
+  history <- data %>%
+    filter(Date < lubridate::today()) %>%
+    select(Date, Home, Away, HomeGoals, AwayGoals)
+
+  write.csv(fixture, paste0("data/", country, "-fixture.csv"))
+  write.csv(history, paste0("data/", country, ".csv"))
+}
+
+print("All data downloaded")
