a5aea0affa096d19061378f5db139a7fc6bc12ac
artiebits <artur.khusaenov@gmail.com>
2022-05-20 12:25:44 +0200

    replace Russia with France

--- R/footballdata.R
+++ R/footballdata.R
@@ -2,12 +2,12 @@ library(worldfootballR)
 library(lubridate)
 library(dplyr)
 
-countries <- c("ENG", "ESP", "GER", "ITA", "RUS")
+countries <- c("ENG", "ESP", "GER", "ITA", "FRA")
 
 for (country in countries) {
   print(paste("Getting data for", country))
 
-  data <- worldfootballR::get_match_results(country = country, gender = "M", season_end_year = 2010:2022)
+  data <- get_match_results(country = country, gender = "M", season_end_year = 2010:2022)
 
   fixture <- data %>%
     filter(Date >= lubridate::today()) %>%
