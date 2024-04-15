0163ada120aa2ec33237ca09dd755103caf51ef5
artiebits <artur.khusaenov@gmail.com>
2024-04-15 22:16:01 +0200

    update logistic_regression_model_validation.py

--- logistic_regression_model_validation.py
+++ logistic_regression_model_validation.py
@@ -1,27 +1,19 @@
 import numpy as np
-import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score, train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import LabelEncoder
 
 from utils.data_preparation_functions import load_data
 
-data = load_data("match_data/GER.csv")
-data = data[data["Date"] > "2021-10-01"]
-data.to_csv("test.csv")
+data = load_data("data/match_results/ITA.csv")
+data = data[data["Date"] > "2018-08-01"]
 
-features = [
-    "Home",
-    "Away",
-    "Home_xG",
-    "Away_xG",
-    "Referee",
-    # "Venue"
-]
+# Add home/away indicator
+features = ["Home", "Away", "Home_xG", "Away_xG", "Referee", "Venue"]
+
 X = data[features]
-# y = np.where((data["HomeGoals"] > 0) & (data["AwayGoals"] > 0), 1, 0)
 y = np.where((data["HomeGoals"] + data["AwayGoals"] > 2), 1, 0)
 
 club_names_encoder = LabelEncoder()
@@ -30,14 +22,14 @@ venue_encoder = LabelEncoder()
 home_xg_imputer = SimpleImputer()
 away_xg_imputer = SimpleImputer()
 
-X["Home_xG"] = home_xg_imputer.fit_transform(X[["Home_xG"]])
-X["Away_xG"] = away_xg_imputer.fit_transform(X[["Away_xG"]])
 X["Home"] = club_names_encoder.fit_transform(X["Home"])
 X["Away"] = club_names_encoder.transform(X["Away"])
+X["Home_xG"] = home_xg_imputer.fit_transform(X[["Home_xG"]])
+X["Away_xG"] = away_xg_imputer.fit_transform(X[["Away_xG"]])
 X["Referee"] = referee_encoder.fit_transform(X["Referee"])
-# X["Venue"] = venue_encoder.fit_transform(X["Venue"])
+X["Venue"] = venue_encoder.fit_transform(X["Venue"])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
 model = LogisticRegression()
 
