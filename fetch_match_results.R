02c0cab05fae2e03f8ea4ef044e9ea1cee82518f
artiebits <artur.khusaenov@gmail.com>
2024-04-15 21:49:42 +0200

    update data folder structure

--- fetch_match_results.R
+++ fetch_match_results.R
@@ -7,7 +7,7 @@ countries <- c("ENG", "ESP", "GER", "ITA")
 for (country in countries) {
   print(paste("Getting data for", country))
 
-  data <- worldfootballR::get_match_results(country = country, gender = "M", season_end_year = 2010:2024)
+  data <- worldfootballR::fb_match_results(country = country, gender = "M", season_end_year = 2010:2025)
 
   fixture <- data %>%
     filter(Date >= lubridate::today())
@@ -15,8 +15,8 @@ for (country in countries) {
   history <- data %>%
     filter(Date < lubridate::today())
 
-  write.csv(fixture, paste0("data/", country, "-fixture.csv"))
-  write.csv(history, paste0("data/", country, ".csv"))
+  write.csv(fixture, paste0("data/fixtures/", country, ".csv"))
+  write.csv(history, paste0("data/match_results/", country, ".csv"))
 }
 
 print("All data downloaded")
